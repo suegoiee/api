b8a750fb42b320f566007cd047e5cc651c3ef2da
shouwda <dragon19290@gmail.com>
2017-09-21 19:24:46 +0800

    Add cors config.

---
+++ config/cors.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+   
+    'supportsCredentials' => true,
+    'allowedOrigins' => ['*'],
+    'allowedHeaders' => ['DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type'],
+    'allowedMethods' => ['GET, POST, OPTIONS'],
+    'exposedHeaders' => [],
+    'maxAge' => 1728000,
+
+];
