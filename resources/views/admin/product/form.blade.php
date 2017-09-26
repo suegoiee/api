da2ab5a25d04206e0d9e00bf83710033c4a0cf24
shouwda <dragon19290@gmail.com>
2017-09-26 09:37:40 +0800

    Fix product avatar error

--- resources/views/admin/product/form.blade.php
+++ resources/views/admin/product/form.blade.php
@@ -1,8 +1,6 @@
  <ul class="nav nav-tabs" role="tablist">
     <li role="presentation" class="nav-item"><a class="nav-link active" href="#product_tab" aria-controls="product" role="tab" data-toggle="tab">{{trans($module_name.'.admin.product_tab')}}</a></li>
     <li role="presentation" class="nav-item"><a class="nav-link" href="#avatar_tab" aria-controls="avatar" role="tab" data-toggle="tab">{{trans($module_name.'.admin.avatar_tab')}}</a></li>
-    <!--<li role="presentation" class="nav-item"><a class="nav-link" href="#avatar_small_tab" aria-controls="avatar_small" role="tab" data-toggle="tab">{{trans($module_name.'.admin.avatar_small_tab')}}</a></li>
-    <li role="presentation" class="nav-item"><a class="nav-link" href="#avatar_detail_tab" aria-controls="avatar_detail" role="tab" data-toggle="tab">{{trans($module_name.'.admin.avatar_detail_tab')}}</a></li>-->
 </ul>
 <div class="tab-content py-3">
     <div role="tabpanel" class="tab-pane fade show active" id="product_tab">
@@ -10,11 +8,5 @@
     </div>
      <div role="tabpanel" class="tab-pane fade" id="avatar_tab"> 
         @include('admin.product.avatar_form')
-    </div>
-    <div role="tabpanel" class="tab-pane fade" id="avatar_small_tab"> 
-        @include('admin.product.avatar_small_form')
-    </div>
-    <div role="tabpanel" class="tab-pane fade" id="avatar_detail_tab">
-        @include('admin.product.avatar_detail_form')
     </div>
 </div>
