5c2033b1bf99be5860c3a15f0595c7f793e79282
shouwda <dragon19290@gmail.com>
2017-10-05 12:43:19 +0800

    Adjust admin user manager layout

--- resources/views/admin/detail.blade.php
+++ resources/views/admin/detail.blade.php
@@ -3,17 +3,15 @@
     <link rel="stylesheet" href="{{asset('css/detail.css')}}">
 @endsection
 @section('content')
-<div id="content" class="container pt-5">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="{{url('/admin/'.str_plural($module_name))}}"><span class="">{{trans($module_name.'.admin.title')}}</span></a></li>
-        <li class="breadcrumb-item active">
-            <span class="">
-                {{$data ? $title_field:''}}
-            </span>
-        </li>
-    </ol>
-        @include('admin.'.$module_name.'.detail')
-</div>
+<ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="{{url('/admin/'.str_plural($module_name))}}"><span class="">{{trans($module_name.'.admin.title')}}</span></a></li>
+    <li class="breadcrumb-item active">
+        <span class="">
+            {{$data ? $title_field:''}}
+        </span>
+    </li>
+</ol>
+@include('admin.'.$module_name.'.detail')
 @endsection
 
 @section('javascript')
