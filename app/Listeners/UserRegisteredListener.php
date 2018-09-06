cc0a3123da9231856842156e40adfc53d0b7467b
shouwda <dragon19290@gmail.com>
2018-09-06 20:12:28 +0800

    Add register event that assigned product 42 to new user

---
+++ app/Listeners/UserRegisteredListener.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserRegistered;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserRegisteredListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Registered  $event
+     * @return void
+     */
+    public function handle(UserRegistered $event)
+    {
+        $user = $event->user; 
+        $token = $event->token;
+        $http = new \GuzzleHttp\Client;
+        $response = $http->request('post',url('/user/products'),[
+                'headers'=>[
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer '.$token['access_token'],
+                ],
+                'form_params' => [
+                    'products' => [['id'=>42,'quanatity'=>1]],
+                    'user_id' => $user['id'],
+                ],
+            ]);
+    }
+}
