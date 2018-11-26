f75e1bc24592d27ce9508108bd0f6de4111de434
shouwda <dragon19290@gmail.com>
2018-11-26 15:03:08 +0800

    Revert "Merge branch 'develop' into 'master'"
    This reverts commit c164d0af1a9334f334c32cb034b6313b4e978be0, reversing
    changes made to 8d9a3ff62fc9106bb06da2145c5c228431dd91bb.

--- app/Traits/OauthToken.php
+++ app/Traits/OauthToken.php
@@ -2,8 +2,6 @@
 namespace App\Traits;
 use Illuminate\Support\Facades\DB;
 use Laravel\Passport\PersonalAccessClient;
-use Illuminate\Support\Facades\Route;
-
 trait OauthToken
 {
     protected function clientCredentialsGrantToken(){
@@ -22,7 +20,6 @@ trait OauthToken
     }
 
     protected function passwordGrantToken($request){
-        /*
         $http = new \GuzzleHttp\Client;
         $client = PersonalAccessClient::first()->client;
         $oauth_client = $this->getPasswordGrantClient();
@@ -38,27 +35,9 @@ trait OauthToken
         ]);
 
         return json_decode((string) $response->getBody(), true);
-        */
-        $client = PersonalAccessClient::first()->client;
-        $oauth_client = $this->getPasswordGrantClient();
-        $request->request->add([
-            "grant_type" => "password",
-            "username" => $request->input('email'),
-            "password" => $request->input('password'),
-            "client_id"     => $oauth_client->id,
-            "client_secret" => $oauth_client->secret,
-        ]);
-        $tokenRequest = $request->create(
-            env('APP_URL').'/oauth/token',
-            'post'
-        );
-        $instance = Route::dispatch($tokenRequest);
-
-        return json_decode($instance->getContent(), true);
     }
 
     protected function refreshGrantToken($request){
-        /*
         $http = new \GuzzleHttp\Client;
         $client = PersonalAccessClient::first()->client;
         $oauth_client = $this->getPasswordGrantClient();
@@ -73,23 +52,6 @@ trait OauthToken
         ]);
 
         return json_decode((string) $response->getBody(), true);
-        */
-        $client = PersonalAccessClient::first()->client;
-        $oauth_client = $this->getPasswordGrantClient();
-        $request->request->add([
-            'grant_type' => 'refresh_token',
-            'client_id' =>  $oauth_client->id,
-            'client_secret' => $oauth_client->secret,
-            'refresh_token' => $request->input('refresh_token'),
-            'scope' =>  '',
-        ]);
-        $tokenRequest = $request->create(
-            env('APP_URL').'/oauth/token',
-            'post'
-        );
-        $instance = Route::dispatch($tokenRequest);
-
-        return json_decode($instance->getContent(), true);
     }
     
     private function getPasswordGrantClient(){
@@ -97,4 +59,3 @@ trait OauthToken
         return $client;
     }
 }
-
