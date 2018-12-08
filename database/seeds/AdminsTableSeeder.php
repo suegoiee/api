191bfd6c4a6723e78b44bf4e352c275583da5b6d
shouwda <dragon19290@gmail.com>
2018-12-08 20:05:02 +0800

    change admin password

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -7,10 +7,11 @@ class AdminsTableSeeder extends Seeder
 {
     public function run()
     {
+    	DB::table('admins')->truncate();
     	if(DB::table('admins')->count()==0){
 	        DB::table('admins')->insert([
 	            'name' => 'uanalyze',
-	            'password' => Hash::make('123456'),
+	            'password' => Hash::make('57f0a7008635d0a0fe7a6c9335185020'),
 	            'auth' => 0
 	        ]);
      	}
