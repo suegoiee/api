e982d61c931c5e44d403d2789212031021f86cbc
shouwda <dragon19290@gmail.com>
2019-04-22 19:07:25 +0800

    Fix article import to forum

--- app/Article.php
+++ app/Article.php
@@ -14,9 +14,36 @@ class Article extends UanalyzeModel
     protected $fillable = [
         'title','content','top','posted_at','status','slug'
     ];
-
+    private function id(){
+    	return $this->id;
+    }
     public function tags()
     {
         return $this->morphToMany('App\Tag', 'taggable')->withTimestamps();
+    }
+
+    public function generateUniqueSlug()
+    {
+    	$value = $this->slug;
+        $slug = $originalSlug = str_slug($value);
+        $counter = 0;
+
+        while ($this->slugExists($slug, $this->exists ? $this->id() : null)) {
+            $counter++;
+            $slug = $originalSlug.'-'.$counter;
+        }
+
+        return $slug;
+    }
+
+    public function slugExists(string $slug, int $ignoreId = null): bool
+    {
+        $query = $this->where('slug', $slug);
+
+        if ($ignoreId) {
+            $query->where('id', '!=', $ignoreId);
+        }
+
+        return $query->count();
     }
 }
