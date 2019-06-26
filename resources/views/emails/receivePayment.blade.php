fef62ddfccf7b115d2bb12e1c3e0c7dac281ea1e
shouwda <dragon19290@gmail.com>
2019-06-26 17:18:56 +0800

    modify payement email content

--- resources/views/emails/receivePayment.blade.php
+++ resources/views/emails/receivePayment.blade.php
@@ -3,8 +3,8 @@
 	付款確認通知
 @endsection
 @section('content')
-	<p>感謝您訂購優分析商品，我們已於({{$payment_date}})收到您的付款資料(訂單編號{{$order_no}})
-系統會自動開通您購買策略，<a href="{{env('APP_FRONT_URL','https://pro.uanalyze.com.tw')}}" target="_blank">登入帳號</a>即可立即使用。</p>
+	<p>感謝您訂購優分析商品，我們已於({{$payment_date}})收到您的付款資料(訂單編號{{$order_no}})</p>
+	<p>系統會自動開通您購買策略，<a href="{{env('APP_FRONT_URL','https://pro.uanalyze.com.tw')}}" target="_blank">登入帳號</a>即可立即使用。</p>
 
     <p style="margin-bottom: 0;">投資順利</p>
 @endsection
