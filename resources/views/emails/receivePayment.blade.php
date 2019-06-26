58eaaaf5c4c3d5ec50abc820cd01a97c1588a03e
shouwda <dragon19290@gmail.com>
2019-06-26 19:24:51 +0800

    Fix  payment notification email content

--- resources/views/emails/receivePayment.blade.php
+++ resources/views/emails/receivePayment.blade.php
@@ -4,8 +4,11 @@
 @endsection
 @section('content')
 	<p>感謝您訂購優分析商品，我們已於({{$payment_date}})收到您的付款資料(訂單編號{{$order_no}})</p>
-	<p>系統會自動開通您購買策略，<a href="{{env('APP_FRONT_URL','https://pro.uanalyze.com.tw')}}" target="_blank">登入帳號</a>即可立即使用。</p>
-
+	<p>系統會自動開通您購買的模組，並顯示於"我的達人策略"裡，<a href="{{env('APP_FRONT_URL','https://pro.uanalyze.com.tw')}}" target="_blank">登入帳號</a>即可立即使用。</p>
+	<p>操作方式如下圖所示：</p>
+	<div style="width:100%;padding:5px;">
+		<img src="{{url('images/PaymentReceive_pic.jpg')}}" style="max-width: 100%">
+	</div>
     <p style="margin-bottom: 0;">投資順利</p>
 @endsection
 @section('btn_start')
