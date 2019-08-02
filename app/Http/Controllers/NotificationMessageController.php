cc6d03a51b78af79f6d18ac93b5a388f9807958e
shouwda <dragon19290@gmail.com>
2019-08-02 14:38:32 +0800

    Remove subscription from notification type

--- app/Http/Controllers/NotificationMessageController.php
+++ app/Http/Controllers/NotificationMessageController.php
@@ -122,8 +122,8 @@ class NotificationMessageController extends Controller
         $notificationMessage = $this->notificationMessageRepository->update($id,$request_data);
         
         $users = count($user_ids) > 0 ? 
-                    $this->userRepository->getsWith([],['mail_verified_at.<>'=>null,'id.in'=>$user_ids,'subscription'=>1]) : 
-                    $this->userRepository->getsWith([],['mail_verified_at.<>'=>null,'subscription'=>1]) ;
+                    $this->userRepository->getsWith([],['mail_verified_at.<>'=>null,'id.in'=>$user_ids]) : 
+                    $this->userRepository->getsWith([],['mail_verified_at.<>'=>null]) ;
         if($notificationType=='MassiveAnnouncement'){
             $bcc = [];
             foreach ($users as $key => $user) {
