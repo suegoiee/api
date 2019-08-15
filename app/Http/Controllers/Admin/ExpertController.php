8f943818c9ffcbe49e011e27dcd223489ffc9183
Joshhuang <joshhuang@uanalyze.com.tw>
2019-08-15 16:43:15 +0800

    expert image upload

--- app/Http/Controllers/Admin/ExpertController.php
+++ app/Http/Controllers/Admin/ExpertController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Lang;
+use App\Traits\ImageStorage;
 use App\Repositories\TagRepository;
 use App\Repositories\UserRepository;
 use App\Repositories\ExpertRepository;
 
 class ExpertController extends AdminController
 {
+    use ImageStorage;
+
     public function __construct(Request $request, TagRepository $tagRepository, ExpertRepository $expertRepository, UserRepository $userRepository)
     {
         parent::__construct($request);
@@ -52,6 +55,10 @@ class ExpertController extends AdminController
     {
         $validator = $this->referrerCreateValidator($request->all(), null);
         $request_data = $request->only(['expert_name', 'investment_style', 'introduction', 'investment_period', 'book', 'interview', 'user_id', 'experience', 'avatar']);
+        if($request->file('avatar')){
+            $path = $this->storeImage($request->file('avatar'), 'online_course');
+            $request_data['avatar'] = $path;
+        }
         $referrer = $this->expertRepository->create($request_data);
         $tags = $request->input('tags',[]);
         $referrer->tags()->attach($tags);
@@ -68,6 +75,16 @@ class ExpertController extends AdminController
             return redirect()->back()->withInput($request->all())->withErrors($validator);
         }
         $request_data = $request->only(['expert_name', 'investment_style', 'introduction', 'investment_period', 'book', 'interview', 'user_id', 'experience', 'avatar']);
+        if($request->file('avatar')){
+            $path = $this->storeImage($request->file('avatar'), 'online_course');
+            $request_data['avatar'] = $path;
+        }
+        elseif($request->has('delete_avatar') && !$request->file('avatar')){
+            $request_data['avatar'] = '';
+        }
+        else{
+            $request_data = $request->only(['expert_name', 'investment_style', 'introduction', 'investment_period', 'book', 'interview', 'user_id', 'experience']);
+        }
         //dd($request_data, $id);
         $referrer = $this->expertRepository->update($id, $request_data);
         $tags = $request->input('tags',[]);
@@ -94,10 +111,10 @@ class ExpertController extends AdminController
         return Validator::make($data, [
             'expert_name' => 'required|string',
             'investment_style' => 'int',
-            'introduction' => 'string',
-            'investment_period' => 'string',
-            'interview' => 'string',
-            'book' => 'string',
+            'introduction',
+            'investment_period',
+            'interview',
+            'book',
             'tags' => 'array',
             'user_id',
             /*'interested' => 'int',*/
@@ -108,15 +125,13 @@ class ExpertController extends AdminController
     {
         return Validator::make($data, [
             'expert_name' => 'required|string',
-            'quota' => 'int',
-            'introduction' => 'string',
-            'host' => 'string',
-            'suitable' => 'string',
+            'investment_style' => 'int',
+            'introduction',
+            'investment_period',
+            'interview',
+            'book',
             'tags' => 'array',
-            'experts' => 'array',
-            'image' => 'string',
             'user_id',
-            /*'interested' => 'int',*/
         ]);        
     }
 }
