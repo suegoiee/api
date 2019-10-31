c5a3119cc001ea503670dd4cea583f548926df59
shouwda <dragon19290@gmail.com>
2019-10-31 10:37:00 +0800

    Fix lab by same pathname

--- app/Repositories/LaboratoryRepository.php
+++ app/Repositories/LaboratoryRepository.php
@@ -3,8 +3,9 @@ namespace App\Repositories;
 use App\Laboratory;
 class LaboratoryRepository extends Repository
 {
-	public function __construct(Laboratory $laboratory){
-		$this->model = $laboratory;
+	public function __construct(Laboratory $model){
+		$this->model = $model;
+		$this->condition = $model;
 	}
 	public function isOwner($user_id,$id){
 		return (boolean)$this->model->where('user_id',$user_id)->where('id',$id)->count();
