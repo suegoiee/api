760c0ed51215037db4a7f1304880d2d35830f78a
shouwda <dragon19290@gmail.com>
2019-11-29 13:58:19 +0800

    relpace new line in html in announcement content

--- app/Announcement.php
+++ app/Announcement.php
@@ -9,4 +9,8 @@ class Announcement extends UanalyzeModel
     protected $fillable = [
         'title','content','type','status'
     ];
+    public function getContentAttribute($value)
+    {
+        return preg_replace('/\r?\n/i', '<br/>', $value);
+    }
 }
