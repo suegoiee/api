93c5d9f00f204f9b64082894b098f4a19949ac4f
shouwda <dragon19290@gmail.com>
2019-12-04 16:12:04 +0800

    add log dont report

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -25,7 +25,7 @@ class Handler extends ExceptionHandler
         \Illuminate\Database\Eloquent\ModelNotFoundException::class,
         \Illuminate\Session\TokenMismatchException::class,
         \Illuminate\Validation\ValidationException::class,
-        \Illuminate\Auth\AuthenticationException:class,
+        \Illuminate\Auth\AuthenticationException::class,
     ];
 
     /**
